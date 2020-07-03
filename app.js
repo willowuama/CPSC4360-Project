5e050f9170ad7964228de8df50b323226de748dc
willowuama <willowuama@gmail.com>
2020-07-03 15:00:25 -0500

    add views

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 // Setting up server
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 const port = 3000;
 
 // Database
@@ -16,6 +17,24 @@ const port = 3000;
 // Models
 
 // Test Data
+
+// Home Routing
+app.route('/')
+.get((req, res) => {
+  res.render('home');
+});
+
+// Login Routing
+app.route('/login')
+.get((req, res) => {
+  res.render('login');
+});
+
+// Register Routing
+app.route('/register')
+.get((req, res) => {
+  res.render('register');
+});
 
 // Launch Server
 app.listen(port, () => {
