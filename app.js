bb8af5098a9879f001fbc2f237dd6d584081a255
willowuama <willowuama@gmail.com>
2020-07-04 03:50:49 -0500

    add db connection. create db shemas

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 // Setting up server
 const app = express();
@@ -10,11 +11,40 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 const port = 3000;
 
-// Database
+//  Database configuration
+const dbURL = "mongodb://localhost:27017/covidDB"
+mongoos.connect(dbURL, {useNewUrlParser: true, useUnifiedTopology: true});
 
 // Schemas
+// Admin Schema
+const adminSchema = new Schema({
+  email: String,
+  password: String,
+  first_name: String,
+  last_name: String
+});
+
+// User Schema
+const userSchema = new Schema({
+  email: String,
+  password: String,
+  first_name: String,
+  last_name: String
+});
+
+// Report Schema
+const reportSchema = new Schema({
+  title: String
+});
 
 // Models
+// Admin Model
+const Admin = new mongoose.model('Admin', adminSchema)
+
+// User Model
+const User = new mongoose.model('User', userSchema)
+
+// Report Model
 
 // Test Data
 
