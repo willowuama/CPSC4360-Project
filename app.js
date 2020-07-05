1f2c521e628f08ab3dd8921d1f228c33cdf4c770
willowuama <willowuama@gmail.com>
2020-07-05 15:36:24 -0500

    add register and login functionality

--- app.js
+++ app.js
@@ -71,18 +71,63 @@ app.route('/')
 app.route('/login')
 .get((req, res) => {
   res.render('login');
+})
+.post((req, res) => {
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, (err, foundUser) => {
+    if(err){
+      console.log(err);
+      res.redirect('/login');
+    }else{
+      if(foundUser){
+        if(foundUser.password === password){
+          res.redirect('report');
+        }else{
+          res.redirect('/login')
+        }
+      }else{
+        res.redirect('/login')
+      }
+    }
+  })
+
 });
 
 // Register Routing
 app.route('/register')
 .get((req, res) => {
   res.render('register');
+})
+.post((req, res) => {
+  const newUser = new User({
+    email: req.body.email,
+    password: req.body.password,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name
+  });
+
+  newUser.save((err) => {
+    if(!err){
+      res.redirect('/login');
+    }else{
+      res.send(err);
+    }
+  })
 });
 
 // Admin Login Routing
 app.route('/admin')
 .get((req, res) => {
   res.render('admin');
+});
+
+// Report Routing
+app.route('/report')
+.get((req, res) => {
+  res.render('report');
 });
 
 // Launch Server
