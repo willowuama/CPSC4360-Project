b939d9d90bd17c2ef76edbb4a0102a21cd43afc7
willowuama <willowuama@gmail.com>
2020-07-05 20:05:23 -0500

    mvp complete

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const date = require(__dirname + "/date.js");
 const https = require("https");
+const covid = require(__dirname + "/covid.js");
 
 // Setting up server
 const app = express();
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 const port = 3000;
@@ -53,7 +55,12 @@ const reportSchema = new Schema({
   slope: Number,
   positive: Number,
   recovered: Number,
-  mutation: Number
+  mutation: String
+});
+
+// News Schema
+const newsSchema = new Schema({
+  url: String
 });
 
 // Models
@@ -65,6 +72,9 @@ const User = new mongoose.model('User', userSchema)
 
 // Report Model
 const Report = new mongoose.model('Report', reportSchema)
+
+// News Model
+const News = new mongoose.model('News', newsSchema)
 
 // Creating an admin
 
@@ -171,12 +181,105 @@ app.route('/adminpage')
 // Report Routing
 app.route('/report')
 .get((req, res) => {
-  res.render('report');
+
+  Report.find({}, (err, foundItems) => {
+    res.render('report', {reportList: foundItems})
+  });
+
 });
+
+// Create Report Routing
+app.route('/createreport')
+.get((req, res) => {
+  res.render('createreport');
+})
+.post((req, res) => {
+
+  const mutation = req.body.mutation;
+  const covidURL = "https://api.covid19api.com/summary";
+  const options = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  https.get(covidURL, options, (response) => {
+    var body = '';
+    response.on('data', (chunk) => {
+      body = body + chunk;
+    })
+
+    response.on('end', () => {
+      const covidData = JSON.parse(body);
+      console.log(covidData.Global.TotalConfirmed);
+
+      const newReport = new Report({
+        date: date.getDate(),
+        daily_probability: covid.getProbability(covidData.Global.NewConfirmed, covidData.Global.TotalConfirmed),
+        slope: covid.getSlope(covidData.Global.TotalConfirmed, covidData.Global.TotalRecovered),
+        positive: covidData.Global.TotalConfirmed,
+        recovered: covidData.Global.TotalRecovered,
+        mutation: mutation
+      });
+
+      newReport.save((err) => {
+        if(!err){
+          res.redirect("/adminpage");
+        }else{
+          res.send(err);
+        }
+      })
+
+    })
+
+  })
+
+});
+
+// Add News route
+app.route('/addnews')
+.get((req, res) => {
+  res.render('addnews');
+})
+.post((req, res) => {
+
+  const newsURL = new News({
+    url: req.body.news
+  });
+
+  newsURL.save((err) => {
+    if(!err){
+      res.redirect('/adminpage');
+    }else{
+      res.redirect('/addnews');
+    }
+  })
+
+});
+
+// News route
+app.route('/news')
+.get((req, res) => {
+  Report.find({}, (err, foundItems) => {
+    res.render('news', {newsList: foundItems})
+  });
+});
+
+// Individual report render
+app.get('/report/:reportid', (req, res) => {
+  const reportID = req.params.reportid;
+
+  Report.findOne({_id: reportID}, (err, foundReport) => {
+    if(!err){
+      res.render('reportpage', {report: foundReport});
+    }else{
+      res.send(err)
+    }
+  })
+
+})
 
 // Launch Server
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
-  //today = date.getDate();
-  //console.log(`Todays date is: ${today}`);
 })
---
+++ covid.js
@@ -0,0 +1,9 @@
+//jshint esversion:6
+
+exports.getProbability = function(x, y) {
+  return (x/y)
+}
+
+exports.getSlope = function(x, y){
+  return (x/y)
+}
